450fe5f4830be548845844ef1ecbc4dc0e03067c
msubhan33 <msubhan9803@gmail.com>
2022-04-24 14:49:53 +0500

    Installed graphql; deleted unused images; installed libraries

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,27 @@
 import "react-circular-progressbar/dist/styles.css";
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import { Provider } from 'react-redux';
-import '../public/css/style.css';
-import store from '../redux/store';
+import "react-perfect-scrollbar/dist/css/styles.css";
+import { Provider } from "react-redux";
+import "../public/css/style.css";
+import store from "../redux/store";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
-    )
-  }
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ApolloProvider>
+  );
+}
 
-export default MyApp
+export default MyApp;
